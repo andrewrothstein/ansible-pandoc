04a1c95bb66371464dbab6ca1f9f806fa5839a77
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-02-24 22:17:50 -0500

    pandoc:2.17.1.1

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -18,7 +18,7 @@ dl() {
 
     if [ ! -e $lfile ];
     then
-        wget -q -O $lfile $url
+        curl -sSLf -o $lfile $url
     fi
     printf "    # %s\n" $url
     printf "    %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
@@ -33,5 +33,4 @@ dl_ver() {
     dl $ver windows x86_64 zip
 }
 
-dl_ver 2.16.2
-dl_ver ${1:-2.17.0.1}
+dl_ver ${1:-2.17.1.1}
